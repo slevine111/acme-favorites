26fa453d53ffae084ca91e740cfcc4aea288a679
Steven Levine <slevine111@gmail.com>
2019-03-10 17:41:51 -0400

    putting mock TestApp component to better understand React

--- client/App.js
+++ client/App.js
@@ -4,51 +4,14 @@ import axios from 'axios'
 import Navbar from './Navbar'
 import UsersList from './UsersList'
 import ThingsList from './ThingsList'
+import TestApp from './TestApp'
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      users: [],
-      things: [],
-      numberDataEntries: { users: 0, things: 0 }
-    }
-  }
-
-  componentDidMount() {
-    return Promise.all([axios.get('/users'), axios.get('/things')])
-      .then(([users, things]) => {
-        return {
-          users: users.data,
-          things: things.data,
-          numberDataEntries: {
-            users: users.data.length,
-            things: things.data.length
-          }
-        }
-      })
-      .then(data => this.setState(data))
-      .catch(err => console.error(err))
-  }
-
   render() {
+    console.log('in render')
     return (
       <Router>
-        <div>
-          <h1> Acme Favorites </h1>
-          <Navbar numberDataEntries={this.state.numberDataEntries} />
-          <Switch>
-            <Route
-              path="/users"
-              render={() => <UsersList users={this.state.users} />}
-            />
-            <Route
-              path="/things"
-              render={() => <ThingsList things={this.state.things} />}
-            />
-            <Redirect to="/users" />
-          </Switch>
-        </div>
+        <Route component={TestApp} />
       </Router>
     )
   }
